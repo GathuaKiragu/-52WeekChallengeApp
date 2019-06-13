fe7d140aaaa0084831d288d0c98136d7adfe5350
GathuaKiragu <kiragugathua@gmail.com>
2019-06-13 09:25:02 +0300

    Created Splash Screen Activity

---
+++ app/src/main/java/com/safaricom/hackathon/Ui/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.safaricom.hackathon.Ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.safaricom.hackathon.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
