1290424d6d9192650e28a6959ff9037d6523238f
GathuaKiragu <kiragugathua@gmail.com>
2019-06-13 10:22:58 +0300

    Added splash screen loading functionality

--- app/src/main/java/com/safaricom/hackathon/Ui/SplashScreenActivity.java
+++ app/src/main/java/com/safaricom/hackathon/Ui/SplashScreenActivity.java
@@ -1,15 +1,55 @@
 package com.safaricom.hackathon.Ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.safaricom.hackathon.MainActivity;
 import com.safaricom.hackathon.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends Activity {
+
+    private Timer timer;
+    private ProgressBar progressBar;
+    private int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        progressBar=(ProgressBar)findViewById(R.id.pdialog);
+        progressBar.setProgress(0);
+
+        final long period =30;
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //this repeats every 100 ms
+                if (i<30){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        }
+                    });
+                    progressBar.setProgress(i);
+                    i++;
+                }else{
+                    //closing the timer
+                    timer.cancel();
+                    Intent intent =new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    // close this activity
+                    finish();
+                }
+            }
+        }, 0, period);
     }
+
 }
